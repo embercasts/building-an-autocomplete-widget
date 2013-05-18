a6d8e778fb21649d3c5bfb3483f05dcf3b692d65
Fabricio Tavares de Oliveira <fabriciotavareso@gmail.com>
2013-05-18 16:10:28 -0300

    Add simple highlight on results

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 App = Ember.Application.create();
 
+Ember.Handlebars.registerBoundHelper('highlight', function(searchResult, searchText) {
+  var regexp, strong;
+
+  regexp = new RegExp(searchText, 'i');
+  strong = '<strong>' + searchText + '</strong>';
+
+  return new Handlebars.SafeString(searchResult.replace(regexp, strong));
+});
+
 App.AutocompleteController = Ember.Controller.extend({
   searchText: null,
 
@@ -8,8 +17,8 @@ App.AutocompleteController = Ember.Controller.extend({
     if (!searchText) { return; }
 
     var regex = new RegExp(searchText, 'i');
-    return ['one', 'two', 'three'].filter(function(name) {
-      return name.match(regex);
+    return [{name: 'one'}, {name: 'two'}, {name:'three'}].filter(function(item) {
+      return item.name.match(regex);
     });
   }.property('searchText')
 });
